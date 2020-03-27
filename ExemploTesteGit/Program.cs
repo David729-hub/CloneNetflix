ee6d03bfea5ad6780d0f48c625a0a18c162657e4
David Caron <ccaron.david@outlook.com>
2020-03-27 11:00:10 -0300

    Arquivo Principal foi alterado com uma nova mensagem

--- ExemploTesteGit/Program.cs
+++ ExemploTesteGit/Program.cs
@@ -7,6 +7,7 @@ namespace ExemploTesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Exemplo Git!");
+            Console.WriteLine("Fizemos o Primeiro Commit!");
         }
     }
 }
