de597bea32653880ef3249f40ecf5b3388c792b5
David Caron <ccaron.david@outlook.com>
2020-03-27 10:57:35 -0300

    Commit Inicial

---
+++ ExemploTesteGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploTesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo Git!");
+        }
+    }
+}
